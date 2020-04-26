90a0afe1e59bca0a55389919977a1f4d533f67ec
bzhao <zhaobin0220xsg@gmail.com>
2020-04-26 11:44:28 +1000

    version10

--- version/app.js
+++ version/app.js
@@ -3,15 +3,15 @@ app = express();
 
 var response;
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+    response = 'This is version 10 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
 
 });
 
-app.listen(8080, function () {
-  console.log('Server listening on port 8080...');
+app.listen(8080, function() {
+    console.log('Server listening on port 8080...');
 });
